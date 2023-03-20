6bf9dfc38e4e4f15bd84bb3ba0815f84752f6c07
Ho Trung <khanhtrung14111999@gmail.com>
2023-03-20 14:59:27 +0200

    Complete setup MUI

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,19 @@
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material';
+import { useSelector } from 'react-redux';
+import { themeSettings } from 'theme';
+import { useMemo } from 'react';
+
 function App() {
-  return <div className='app'></div>;
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]); //pass mode into createTheme, in theme file, themeSetting in the mode and use createTheme to configure neccessary for UI
+  return (
+    <div className='app'>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
+    </div>
+  );
 }
 
 export default App;
