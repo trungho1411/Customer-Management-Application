7f306e898f1a1db6ec632d4cf1dfde04ce34c04e
Ho Trung <khanhtrung14111999@gmail.com>
2023-03-20 16:45:19 +0200

    builidng Navbar with MUI

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,28 @@
 import { CssBaseline, ThemeProvider } from '@mui/material';
-import { createTheme } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
 import { useSelector } from 'react-redux';
 import { themeSettings } from 'theme';
 import { useMemo } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Layout from './scenes/layout';
+import Dashboard from './scenes/dashboard';
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]); //pass mode into createTheme, in theme file, themeSetting in the mode and use createTheme to configure neccessary for UI
   return (
     <div className='app'>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path='/' element={<Navigate to='/dashboard' replace />} />
+              <Route path='/dashboard' element={<Dashboard />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
