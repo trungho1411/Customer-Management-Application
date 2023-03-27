1703646b3bde0b4cba3897bfd7a401bbd61a11e1
Ho Trung <khanhtrung14111999@gmail.com>
2023-03-27 18:55:22 +0300

    make Api call into userID

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -1,29 +1,37 @@
-import React ,{ useState } from 'react'
-import { Box, useMediaQuery } from '@mui/material'
-import { Outlet } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import NavBar from 'components/NavBar'
-import Sidebar from 'components/Sidebar'
+import React, { useState } from 'react';
+import { Box, useMediaQuery } from '@mui/material';
+import { Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import NavBar from 'components/NavBar';
+import Sidebar from 'components/Sidebar';
+import { useGetUserQuery } from 'state/api';
 
 const Layout = () => {
-  const isNonMobile  = useMediaQuery("(min-width: 600px)")
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true)
-  return (
-    <Box display = {isNonMobile ? "flex" : "block"} width = "100%" height="100%">
-      <Sidebar 
-       isNonMobile = {isNonMobile}
-       drawerWidth = "250px"
-       isSidebarOpen = {isSidebarOpen}
-       setIsSidebarOpen = {setIsSidebarOpen}
-       />
-        <Box>
-            <NavBar 
-            isSidebarOpen = {isSidebarOpen}
-            setIsSidebarOpen = {setIsSidebarOpen}/>
-            <Outlet />
-        </Box>
-    </Box>
-  )
-}
+  const isNonMobile = useMediaQuery('(min-width: 600px)');
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const userId = useSelector((state) => state.global.userId);
+  const { data } = useGetUserQuery(userId);
+  console.log('data', data);
 
-export default Layout
+  return (
+    <Box display={isNonMobile ? 'flex' : 'block'} width='100%' height='100%'>
+      <Sidebar
+        user={data || {}}
+        isNonMobile={isNonMobile}
+        drawerWidth='250px'
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
+      <Box>
+        <NavBar
+          user={data || {}}
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+        />
+        <Outlet />
+      </Box>
+    </Box>
+  );
+};
+
+export default Layout;
