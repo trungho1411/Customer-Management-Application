e46b715e35da0a3aff45fcbbfde2adbabac6b2e6
Ho Trung <khanhtrung14111999@gmail.com>
2023-03-27 18:37:35 +0300

    Added .env.local files, created Api.js file, configure index.js with redux in frontend; Added data into index.js file in backend and show data on MongoDB

--- server/index.js
+++ server/index.js
@@ -11,6 +11,10 @@ import generalRoutes from './routes/general.js';
 import managementRoutes from './routes/management.js';
 import salesRoutes from './routes/sales.js';
 
+/*import data*/
+import User from './models/User.js';
+import { dataUser } from './data/index.js';
+
 /*CONFIGURATION*/
 
 dotenv.config();
@@ -38,5 +42,8 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    //one time adding data
+    User.insertMany(dataUser);
   })
   .catch((error) => console.log(`${error} did not connect`));
