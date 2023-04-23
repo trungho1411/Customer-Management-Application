b9ccdb299701e9c8516099fd43f27f65436a45ea
Ho Trung <khanhtrung14111999@gmail.com>
2023-04-24 01:12:19 +0300

    Change title

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -132,7 +132,7 @@ const Sidebar = ({
               <FlexBetween color={theme.palette.secondary.main}>
                 <Box display='flex' alignItems='center' gap='0.5rem'>
                   <Typography variant='h4' fontWeight='bold'>
-                    Thesis
+                    Customer Manager App
                   </Typography>
                 </Box>
                 {!isNonMobile && (
