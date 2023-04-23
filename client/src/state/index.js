bb96b71ec468fcf7a975bdd1fab65be3250e139a
Ho Trung <khanhtrung14111999@gmail.com>
2023-04-24 00:59:07 +0300

    replace new user

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -2,7 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   mode: 'dark',
-  userId: '63701cc1f03239d59100031c',
+  userId: '63701cc1f03239d59100031b',
 };
 
 export const globalSlice = createSlice({
